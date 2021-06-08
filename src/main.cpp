ecc4c6f55f039b7ceaf6a6157e23c07ea2efaf9f
Liam Tyler <tyler.liam7@gmail.com>
2021-06-07 21:51:22 -0500

    fixed up cmake, apparently execute_process isn't sequential. Also added libsndfile, with example wav opened and played back. Linux still not tested

--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,59 @@
 #include "utils/logger.hpp"
-
-#include <stdio.h>
-#include <math.h>
 #include "portaudio.h"
+#include "sndfile.h"
+#include <vector>
 
-#define NUM_SECONDS         (1)
-#define SAMPLE_RATE         (44100)
-#define FRAMES_PER_BUFFER   (1024)
 
-#ifndef M_PI
-#define M_PI  (3.14159265)
-#endif
-
-#define TABLE_SIZE   (200)
-
-int main(void)
+int main( int argc, char** argv )
 {
     Logger_Init();
     Logger_AddLogLocation( "stdout", stdout );
-    
+
+    char *inFileName;
+    SNDFILE *inFile;
+    SF_INFO inFileInfo;
+    int fs;
+
+    inFileName = "sm64_mario_hoo.wav";
+
+    inFile = sf_open(inFileName, SFM_READ, &inFileInfo);
+    if ( !inFile )
+    {
+        LOG_ERR( "Could not load WAV file" );
+        return 0;
+    }
+    std::vector<float> buffer( inFileInfo.frames * inFileInfo.channels );
+    sf_read_float( inFile, buffer.data(), inFileInfo.frames * inFileInfo.channels );
+    sf_close(inFile);
+
+    LOG( "Sample Rate = %d Hz", inFileInfo.samplerate );
+    LOG( "Channels = %d Hz", inFileInfo.channels );
+    LOG( "frames = %d Hz", inFileInfo.frames );
+    printf( "Format %x\n", inFileInfo.format );
+
     PaStreamParameters outputParameters;
     PaStream *stream;
     PaError err;
-    float buffer[FRAMES_PER_BUFFER][2]; /* stereo output buffer */
-    float sine[TABLE_SIZE]; /* sine wavetable */
-    int left_phase = 0;
-    int right_phase = 0;
-    int left_inc = 2;
-    int right_inc = 3; /* higher pitch so we can distinguish left and right. */
-    int i, j, k;
-    int bufferCount;
-
-    printf("PortAudio Test: output sine wave. SR = %d, BufSize = %d\n", SAMPLE_RATE, FRAMES_PER_BUFFER);
-
-    /* initialise sinusoidal wavetable */
-    for( i=0; i<TABLE_SIZE; i++ )
-    {
-        sine[i] = (float) sin( ((double)i/(double)TABLE_SIZE) * M_PI * 2. );
-    }
-
+    //float buffer[FRAMES_PER_BUFFER][2]; /* stereo output buffer */
 
     LOG_WARN( "PRE init" );
     err = Pa_Initialize();
     LOG_WARN( "Past init" );
-    if( err != paNoError ) goto error;
-
-    outputParameters.device = Pa_GetDefaultOutputDevice(); /* default output device */
-    if (outputParameters.device == paNoDevice) {
-        fprintf(stderr,"Error: No default output device.\n");
-        goto error;
+    if ( err != paNoError )
+    {
+        LOG_ERR( "Failed to initialize portaudio" );
+        return 0;
     }
-    outputParameters.channelCount = 2;       /* stereo output */
+
+    outputParameters.device = Pa_GetDefaultOutputDevice();
+    if ( outputParameters.device == paNoDevice )
+    {
+        LOG_ERR( "No default output device." );
+        return 0;
+    }
+    LOG( "default low output latency: %f", Pa_GetDeviceInfo( outputParameters.device )->defaultLowOutputLatency );
+
+    outputParameters.channelCount = inFileInfo.channels;       /* stereo output */
     outputParameters.sampleFormat = paFloat32; /* 32 bit floating point output */
     outputParameters.suggestedLatency = 0.050; // Pa_GetDeviceInfo( outputParameters.device )->defaultLowOutputLatency;
     outputParameters.hostApiSpecificStreamInfo = NULL;
@@ -59,79 +62,28 @@ int main(void)
               &stream,
               NULL, /* no input */
               &outputParameters,
-              SAMPLE_RATE,
-              FRAMES_PER_BUFFER,
+              inFileInfo.samplerate,
+              inFileInfo.frames,
               paClipOff,      /* we won't output out of range samples so don't bother clipping them */
               NULL, /* no callback, use blocking API */
               NULL ); /* no callback, so no callback userData */
-    if( err != paNoError ) goto error;
-
-
-    printf( "Play 3 times, higher each time.\n" );
-
-    for( k=0; k < 3; ++k )
+    if ( err != paNoError )
     {
-        err = Pa_StartStream( stream );
-        if( err != paNoError ) goto error;
-
-        printf("Play for %d seconds.\n", NUM_SECONDS );
-
-        bufferCount = ((NUM_SECONDS * SAMPLE_RATE) / FRAMES_PER_BUFFER);
-
-        for( i=0; i < bufferCount; i++ )
-        {
-            for( j=0; j < FRAMES_PER_BUFFER; j++ )
-            {
-                buffer[j][0] = sine[left_phase];  /* left */
-                buffer[j][1] = sine[right_phase];  /* right */
-                left_phase += left_inc;
-                if( left_phase >= TABLE_SIZE ) left_phase -= TABLE_SIZE;
-                right_phase += right_inc;
-                if( right_phase >= TABLE_SIZE ) right_phase -= TABLE_SIZE;
-            }
-
-            err = Pa_WriteStream( stream, buffer, FRAMES_PER_BUFFER );
-            if( err != paNoError ) goto error;
-        }
-
-        err = Pa_StopStream( stream );
-        if( err != paNoError ) goto error;
-
-        ++left_inc;
-        ++right_inc;
-
-        Pa_Sleep( 1000 );
+        LOG_ERR( "Could not open audio stream" );
+        return 0;
     }
+
+    err = Pa_StartStream( stream );
+    err = Pa_WriteStream( stream, buffer.data(), buffer.size() );
+    LOG_WARN(" After write" );
+    Pa_Sleep( 1000 );
+    LOG_WARN( "after sleep" );
+    
+    err = Pa_StopStream( stream );
 
     err = Pa_CloseStream( stream );
-    if( err != paNoError ) goto error;
 
     Pa_Terminate();
-    printf("Test finished.\n");
 
-    return err;
-
-error:
-    LOG_ERR( "PortAudioError!" );
-    fprintf( stderr, "An error occurred while using the portaudio stream\n" );
-    fprintf( stderr, "Error number: %d\n", err );
-    fprintf( stderr, "Error message: %s\n", Pa_GetErrorText( err ) );
-    // Print more information about the error.
-    if( err == paUnanticipatedHostError )
-    {
-        const PaHostErrorInfo *hostErrorInfo = Pa_GetLastHostErrorInfo();
-        fprintf( stderr, "Host API error = #%ld, hostApiType = %d\n", hostErrorInfo->errorCode, hostErrorInfo->hostApiType );
-        fprintf( stderr, "Host API error = %s\n", hostErrorInfo->errorText );
-    }
-    Pa_Terminate();
-    return err;
+    return 0;
 }
-
-
-// int main( int argc, char** argv )
-// {
-//     Logger_Init();
-//     Logger_AddLogLocation( "stdout", stdout );
-//     LOG( "Hello world!" );
-//     return 0;
-// }
